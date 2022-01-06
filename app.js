673890919595c3029d22642ed21a86c1a7353b9a
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-01-06 17:38:41 +0500

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const StripeSecretKey = process.env.STRIPE_SECRET_KEY
 const StripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
 const express = require("express")
-const fs = require("fs")
+const fs = require("fs") 
 const app = express()
 
 app.set("view engine", "ejs")
@@ -15,7 +15,7 @@ app.use(express.static("public"))
 app.listen(5000, () => console.log("Everything is good!"))
 
 app.get("/store", (req, res) =>{
-    fs.readFile("data.json", (e, data) => {
+    fs.readFile("data.json", (e, data) => { 
         if(e){
             res.status(500).end()
         }
