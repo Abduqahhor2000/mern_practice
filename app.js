1412641c3c6f849d06bd0a21872fbdec6f682348
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-01-06 17:21:54 +0500

    practice

---
+++ app.js
@@ -0,0 +1,26 @@
+if( process.env.NODE_env !== "production"){
+    require("dotenv").config({path: ".env"})
+}
+
+const StripeSecretKey = process.env.STRIPE_SECRET_KEY
+const StripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+const express = require("express")
+const fs = require("fs")
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.listen(5000, () => console.log("Everything is good!"))
+
+app.get("/store", (req, res) =>{
+    fs.readFile("data.json", (e, data) => {
+        if(e){
+            res.status(500).end()
+        }
+        else{
+            res.render("store.ejs", {data: JSON.parse(data)})
+        }
+    })
+})
